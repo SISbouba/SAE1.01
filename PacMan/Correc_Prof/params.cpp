5b0814dfc6d316bef1b0f2d3917a18338534d292
Samuel <samuel.celestine@etu.univ-amu.fr>
2025-01-10 09:12:07 +0100

    Update params.cpp

--- PacMan/Correc_Prof/params.cpp
+++ PacMan/Correc_Prof/params.cpp
@@ -1,39 +1,28 @@
 #include <string>
 #include "params.h"
 #include "game.h"
+
 using namespace std;
 
 
-void initParams (CMyParam & Param)
-{
-    //Move Keys
-    Param.MapParamChar["KeyUp"] = 'z';
-    Param.MapParamChar["KeyDown"] = 's';
-
-    Param.MapParamChar["TokenP1"] = 'A';
-    Param.MapParamChar["TokenP2"] = 'X';
-
-    //Size of grid — suppose to be a rectangle
-    Param.MapParamUnsigned["NbColumn"] = 10;
-
-    //Display Colors
-    Param.MapParamString["ColorP1"] = KColor.find("KRed")->second ;
-
-}
 
 void initParams (CMyParamV2 & Param)
-{
-    //Move Keys
+{//Move Keys
     Param.KeyUp = 'z';
     Param.KeyDown = 's';
+    Param.KeyLeft = 'q';
+    Param.KeyRight = 'd';
 
     Param.tokenP1 = 'A';
     Param.tokenP2 = 'X';
 
     //Size of grid — suppose to be a rectangle
     Param.NbColumn = 10;
+    Param.NbRow = 10;
 
     //Display Colors
     Param.ColorP1 = KColor.find("KRed")->second ;
-
+    Param.ColorP2 = KColor.find("KBlue")->second;
 }
+
+Params::Params() {}
