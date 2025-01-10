a51ea3d28d1d9af9df18d7ada1ae8bfaef626385
Samuel <samuel.celestine@etu.univ-amu.fr>
2025-01-10 08:30:41 +0100

    Update gridmanagement.cpp

--- PacMan/Correc_Prof/gridmanagement.cpp
+++ PacMan/Correc_Prof/gridmanagement.cpp
@@ -19,8 +19,7 @@ void Color (const string & Col)
     cout << "\033[" << Col.c_str () <<"m";
 } // Color ()
 
-
-void DisplayGrid (const CMat & Mat)
+void DisplayGrid (const CMat & Mat, const CMyParamV2 & param)
 {
     const unsigned KNbLine = Mat.size ();
     const unsigned KNbCol  = Mat[0].size ();
@@ -42,11 +41,20 @@ void DisplayGrid (const CMat & Mat)
                 Color (KColor.find("KReset")->second);
                 break;
             case 'O':
-                Color (KColor.find("KBlue")->second);
+                Color (KColor.find("KRed")->second);
                 cout << c;
                 Color (KColor.find("KReset")->second);
                 break;
-
+            case '=':
+                Color (KColor.find("KMAgenta")->second);
+                cout << c;
+                Color (KColor.find("KReset")->second);
+                break;
+            case 'T':
+                Color (KColor.find("KOrange")->second);
+                cout << c;
+                Color (KColor.find("KReset")->second);
+                break;
             }
         }
         cout << '|' << endl;
@@ -55,7 +63,7 @@ void DisplayGrid (const CMat & Mat)
 }// ShowMatrix ()
 
 
-void InitGrid (CMat & Mat, unsigned NbLine, unsigned NbColumn, CPosition & PosPlayer1, CPosition & PosPlayer2)
+void InitGrid (CMat & Mat, unsigned NbLine, unsigned NbColumn, CPosition & PosPlayer1, CPosition & PosPlayer2, CPosition & PosMur, CPosition & PosTp, CPosition & PosTp2)
 {
     Mat.resize (NbLine);
     const CVLine KLine (NbColumn, KEmpty);
@@ -68,4 +76,40 @@ void InitGrid (CMat & Mat, unsigned NbLine, unsigned NbColumn, CPosition & PosPl
     PosPlayer2.first = NbLine - 1;
     PosPlayer2.second =0;
     Mat [PosPlayer2.first][PosPlayer2.second]   = 'O';
+
+    PosTp.first = 5;
+    PosTp.second = NbColumn -7;
+    Mat [PosTp.first][PosTp.second] = 'T';
+
+    PosTp2.first = 4;
+    PosTp2.second = NbColumn -2;
+    Mat [PosTp2.first][PosTp2.second] = 'T';
+
+    PosMur.first = 3;
+    PosMur.second = NbColumn -2;
+    Mat [PosMur.first][PosMur.second] = '=';
+    PosMur.first = 3;
+    PosMur.second = NbColumn -1;
+    Mat [PosMur.first][PosMur.second] = '=';
+    PosMur.first = 3;
+    PosMur.second = NbColumn -3;
+    Mat [PosMur.first][PosMur.second] = '=';
+    PosMur.first = 3;
+    PosMur.second = NbColumn -4;
+    Mat [PosMur.first][PosMur.second] = '=';
+
+    PosMur.first = 7;
+    PosMur.second = NbColumn -7;
+    Mat [PosMur.first][PosMur.second] = '=';
+    PosMur.first = 7;
+    PosMur.second = NbColumn -8;
+    Mat [PosMur.first][PosMur.second] = '=';
+    PosMur.first = 7;
+    PosMur.second = NbColumn -9;
+    Mat [PosMur.first][PosMur.second] = '=';
+    PosMur.first = 7;
+    PosMur.second = NbColumn -10;
+    Mat [PosMur.first][PosMur.second] = '=';
 }//InitMat ()
+
+
